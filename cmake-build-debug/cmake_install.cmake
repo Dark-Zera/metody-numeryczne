dff6e005934ca790e179c2aae33cbf405b9700c4
Zera <b.czaicki@getprintbox.com>
2022-03-07 11:45:46 +0100

    Change project name

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Zera/CLionProjects/MN_lab1
+# Install script for directory: C:/Users/Zera/CLionProjects/metody-numeryczne
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/MN_lab1")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/metody-numeryczne")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Zera/CLionProjects/MN_lab1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Zera/CLionProjects/metody-numeryczne/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
